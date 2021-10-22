91a8eed2f1bcfbd072511f67207da395f0cf798d
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-10-22 15:07:21 +0200

    Partie 0 : Introduction

---
+++ coursGerezVotreCatalogueDeProduitsEnPphEtMySqlCRUD/DocBase/global/common/menu.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+    <a class="navbar-brand" href="../../global/accueil/index.php">accueil</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+            <a class="nav-link" href="../../4-cas pratique/2-Catalogue/index.php">Catalogue</span></a>
+        </li>
+    </ul>
+    </div>
+</nav>
